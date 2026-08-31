23d763b7411c9367178331cd5c5bb03181e17840
daniel-wesley <48811416+daniel-wesley@users.noreply.github.com>
2026-08-30 21:37:00 -0300

    chore: preparar deploy para netlify

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/menuuchefe/',
+  base: './',
   plugins: [react()],
   resolve: {
     conditions: ['browser', 'import', 'module', 'default'],
